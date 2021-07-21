059e6dd6b67acc2c948c212ff400e20239793f85
liutongke <liutongke@live.com>
2021-07-21 18:16:10 +0800

    增加数据库访问

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module helloworld
 
 go 1.15
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/mysql v1.1.1
+	gorm.io/gorm v1.21.12
+)
--- main.go
+++ main.go
@@ -3,16 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/add", insert)
 	port := "12223"
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
+
+type User struct {
+	User string `gorm:"column:user" json:"content"` //请求人的userid
+	ID   int    `gorm:"column:id" json:"id"`        //请求人的userid
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!\n")
+	var user User
+	GetMysqlClient().Table("account").First(&user)
+	fmt.Println(user)
+	fmt.Fprintf(w, "Hello World!\n-------------->"+user.User)
+}
+
+func insert(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+
+	user := User{User: time.Now().String(), ID: rand.Intn(100000)}
+
+	GetMysqlClient().Table("account").Create(&user) // 通过数据的指针来创建
+	fmt.Fprintf(w, "add \n-------------->"+strconv.Itoa(user.ID))
+	// 返回插入数据的主键
 }
