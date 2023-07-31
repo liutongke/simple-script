5d290bc48d726948dd88e04c06961310c5cd5a37
keke <liutongke@live.com>
2023-07-31 21:27:05 +0800

    创建redis脚本增加动态接收用户输入容器名称和镜像名称、创建完成后输出redis密码

--- redis/README.md
+++ redis/README.md
@@ -3,7 +3,7 @@ Reids所有版本下载地址：http://download.redis.io/releases
 新创建的容器Redis密码查看命令:
 
 ```
-grep "requirepass" /usr/local/etc/redis/redis.conf
+grep "^requirepass" /usr/local/etc/redis/redis.conf
 ```
 
 ![Img](https://raw.githubusercontent.com/liutongke/Image-Hosting/master/images/yank-note-picgo-img-20230529170417.png)
--- redis/create_redis.py
+++ redis/create_redis.py
@@ -3,6 +3,7 @@
 import os
 import subprocess
 import sys
+import re
 
 
 # 创建目录
@@ -21,19 +22,37 @@ create_directory("log")
 # 获取当前脚本所在的目录路径
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
+# 动态输入镜像名称
+while True:
+    default_image_name = "redis:v1"
+    image_name = input(f"Enter image name (default: {default_image_name}): ")
+    if not image_name.strip():
+        image_name = default_image_name
+    if ":" not in image_name:
+        image_name += ":latest"
+    break
+
 # 构建镜像
-subprocess.run(["docker", "build", "-t", "redis:v1", "./"], check=True)
+subprocess.run(["docker", "build", "-t", image_name, "./"], check=True)
+
+# 动态输入容器名称
+while True:
+    default_container_name = "redis-1"
+    container_name = input(f"Enter container name (default: {default_container_name}): ")
+    if not container_name.strip():
+        container_name = default_container_name
+    break
 
 # 定义数据卷映射
-data_volume = f"{dir_path}/data:/data"
-log_volume = f"{dir_path}/log:/var/log/redis/"
+data_volume = os.path.join(dir_path, "data:/data")
+log_volume = os.path.join(dir_path, "log:/var/log/redis/")
 
 # 运行容器
 subprocess_args = [
-    "docker", "run", "-d", "--name", "redis-1", "-p", "6379:6379", "--restart", "always",
+    "docker", "run", "-d", "--name", container_name, "-p", "6379:6379", "--restart", "always",
     "-v", data_volume,
     "-v", log_volume,
-    "redis:v1"
+    image_name
 ]
 
 if sys.platform.startswith('linux'):
@@ -41,3 +60,17 @@ if sys.platform.startswith('linux'):
     subprocess.run(["sudo", "chmod", "a+rw", "data/", "log/"], check=True)
 
 subprocess.run(subprocess_args, check=True)
+
+
+def run_docker_exec(container_name, command):
+    cmd = f"docker exec {container_name} {command}"
+    result = subprocess.check_output(cmd, shell=True, text=True)
+    return result.strip()
+
+
+grep_command = 'grep "^requirepass" /usr/local/etc/redis/redis.conf'
+
+output = run_docker_exec(container_name, grep_command)
+match = re.search(r"requirepass\s+(.+)", output)
+password = match.group(1)
+print("Redis登录密码:", password)
