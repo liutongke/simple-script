0e3867de94db88cbb9ecdc80f784aac53596efee
keke <liutongke@live.com>
2023-07-31 21:38:11 +0800

    lnmp脚本创建docker网络前判断是否已经存在

--- php8.1-nginx/create_php8.1_nginx.py
+++ php8.1-nginx/create_php8.1_nginx.py
@@ -15,6 +15,14 @@ def create_directory(directory):
         print(directory, "directory already exists in", current_dir)
 
 
+def check_network_exists(network_name):
+    """
+    检查网络是否存在
+    """
+    result = subprocess.run(["docker", "network", "ls", "--format", "{{.Name}}"], capture_output=True, text=True)
+    return network_name in result.stdout.splitlines()
+
+
 # 获取当前目录
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -29,8 +37,12 @@ create_directory(log_dir)
 # 构建 PHP 镜像
 subprocess.run(["docker", "build", "-t", "php:8.1.18-fpm-custom", "."], check=True)
 
-# 创建网络
-subprocess.run(["docker", "network", "create", "lnmp-net"], check=True)
+# 检查 lnmp-net 网络是否存在
+network_name = "lnmp-net"
+if not check_network_exists(network_name):
+    subprocess.run(["docker", "network", "create", network_name], check=True)
+else:
+    print("Network", network_name, "already exists.")
 
 # 运行 nginx 容器
 subprocess.run(["docker", "run", "--name", "nginx-cow", "-itd", "nginx:1.25.0-bullseye"], check=True)
@@ -42,20 +54,20 @@ subprocess.run(["docker", "cp", "nginx-cow:/etc/nginx/conf.d/default.conf", f"{c
 subprocess.run(["docker", "stop", "nginx-cow"], check=True)
 subprocess.run(["docker", "rm", "nginx-cow"], check=True)
 
-# 运行 laravle-nginx-01 容器
+# 运行 laravel-nginx-01 容器
 subprocess.run(
     [
-        "docker", "run", "-itd", "-p", "9600:80", "--network", "lnmp-net", "--network-alias", "lnmp-net",
-        "--name", "laravle-nginx-01", "--restart", "always", "-v", f"{confd_dir}:/etc/nginx/conf.d",
+        "docker", "run", "-itd", "-p", "9600:80", "--network", network_name, "--network-alias", "lnmp-net",
+        "--name", "laravel-nginx-01", "--restart", "always", "-v", f"{confd_dir}:/etc/nginx/conf.d",
         "-v", f"{log_dir}:/var/log/nginx", "-v", f"{current_dir}:/var/www/html", "nginx:1.25.0-bullseye"
     ],
     check=True
 )
 
-# 运行 laravle-php-01 容器
+# 运行 laravel-php-01 容器
 subprocess.run(
     [
-        "docker", "run", "--name", "laravle-php-01", "--network", "lnmp-net", "--network-alias", "lnmp-net",
+        "docker", "run", "--name", "laravel-php-01", "--network", network_name, "--network-alias", "lnmp-net",
         "--restart", "always", "-p", "9601:9000", "-v", f"{current_dir}:/var/www/html", "-d",
         "php:8.1.18-fpm-custom"
     ],
